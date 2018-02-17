9b1754116b09eacfc4e8ea0cb6961e51d7085eb4
deleckie <deleckie@me.com>
2018-02-16 23:43:10 -0500

    time is ticking

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Mike",
+    photo: "https://pbs.twimg.com/profile_images/793303788601749504/Tp4XR3ku_400x400.jpg",
+    scores: [
+      "3",
+      "2",
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "4",
+      "5",
+      "2"
+    ]
+  },
+  {
+    name: "Stan",
+    photo: "http://www.theouthousers.com/images/templates/thumbnails/139909//stanlee_size3.png",
+    scores: [
+      "2",
+      "3",
+      "5",
+      "2",
+      "1",
+      "4",
+      "4",
+      "5",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Sarah",
+    photo: "http://www.sarahjio.com/wp-content/thesis/skins/SarahJio/images/sarah-sidebar2.png",
+    scores: [
+      "2",
+      "3",
+      "1",
+      "4",
+      "5",
+      "2",
+      "2",
+      "5",
+      "1",
+      "2"
+    ]
+  },
+  {
+    name: "Tom",
+    photo: "http://www.tomwaitsfan.com/forums/viewtopic.php?f=1&t=3858&start=165g",
+    scores: [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  },
+  {
+    name: "Perez",
+    photo: "http://americansabor.org/sites/default/files/imagecache/gallery_image_detail_medium/images/pprado1_0.jpg",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Nikola",
+    photo: "https://ancient-code.com/wp-content/uploads/2016/06/Nikola-Tesla-desk.jpg",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
